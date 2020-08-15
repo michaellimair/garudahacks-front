d956b7dfccc7a9a0d96c7dca30ee1e6b1d4b34da
lyulka <strelka@connect.hku.hk>
2020-08-15 18:25:58 +0800

    Create data.js dummy data file.

---
+++ data.js
@@ -0,0 +1,274 @@
+export default {
+  accounts: {
+    adrioalimin: {
+      id: 'adrioalimin',
+      fullName: 'Adrio Alimin',
+      age: 20,
+      educationHistory: [
+        {
+          name: "BEng(CompSc) Computer Science & Philosophy",
+          institution: 'HKU',
+          timeFrom: '2018-08',
+          timeTo: '2022-05', // YYYY-MM
+          about: 'Learn stuff',
+        },
+        {
+          name: "CIE A-Level",
+          timeFrom: '2016-07',
+          timeTo: '2018-06', // YYYY-MM
+          about: 'English, Maths, Physics, Biology',
+        },
+      ],
+      about: 'Interested in Political & Ethical Philosophy. SWE intern.',
+      verified: true,
+      favAccounts: [
+        'michaellim',
+        'cindyeffendy',
+        'vanessagunadi'
+      ],
+      favMeetups: [
+        '2158765541',
+        '6996341243',
+        '7006569730',
+      ],
+    },
+    michaellimair: {
+      id: 'michaellimair',
+      fullName: 'Michael Lim',
+      age: 20,
+      educationHistory: [
+        {
+          name: "BSc CompSc",
+          institution: 'CityU',
+          timeFrom: '2018-08',
+          timeTo: '2022-05', // YYYY-MM
+          about: 'Learn stuff',
+        },
+      ],
+      about: 'Full-time SWE, CityU student',
+      verified: true,
+      favAccounts: [
+        'vanessagunadi'
+      ],
+      favMeetups: [
+        '8474312706',
+        '6996341243',
+        '7006569730',
+      ],
+    },
+  },
+  vanessagunadi: {
+    id: 'vanessagunadi',
+    fullName: 'Vanessa Gunadi',
+    age: 20,
+    educationHistory: [
+      {
+        name: "BBA Professional Accounting",
+        institution: 'HKUST',
+        timeFrom: '2018-08',
+        timeTo: '2022-05', // YYYY-MM
+        about: 'Learn stuff',
+      },
+    ],
+    about: 'Passionate about business',
+    verified: true,
+    favAccounts: [
+      'cindyeffendy'
+    ],
+    favMeetups: [
+      '8474312706',
+      '2286704233',
+      '7006569730',
+    ],
+  },
+  cindyeffendy: {
+    id: 'cindyeffendy',
+    fullName: 'Cindy Effendy',
+    age: 20,
+    educationHistory: [
+      {
+        name: "BBA Business",
+        institution: 'HKU',
+        timeFrom: '2018-08',
+        timeTo: '2022-05', // YYYY-MM
+        about: 'Learn stuff',
+      },
+    ],
+    about: 'Aspiring entrepreneur',
+    verified: false,
+    favAccounts: [
+      'vanessagunadi',
+      'adrioalimin',
+      'michaellimair'
+    ],
+    favMeetups: [
+      '7334275040',
+      '2286704233',
+    ],
+  },
+  meetups: {
+    '2158765541': {
+      title: 'The Politics of Karl Popper',
+      description: `
+      # The first non-justificational philosophy of criticism in the history of philosophy
+
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+      est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
+      modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+      veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+      commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+
+      ## See you there!
+      `,
+      date: '2020-08-16',
+      time: 'T10:00:38.117Z',
+      duration: 90, // in minutes
+      location: 'HKU JCSV III Lap-Chee College',
+      fixed: true,
+      topics: {
+        'social_science': {
+          'linguistics': 'Linguistics',
+          'politics': 'Politics',
+        },
+        'humanities': {
+          'philosophy': 'philosophy'
+        },
+      },
+      password: '1H4S2fRdzcdpfQrK',
+    },
+    '6996341243': {
+      title: 'Chomsky and Universal Grammar',
+      description:  `
+      # The first non-justificational philosophy of criticism in the history of philosophy
+
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+      est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
+      modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+      veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+      commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+
+      ## See you there!
+      `,
+      date: '2020-08-20',
+      time: 'T10:00:38.117Z',
+      duration: 30,
+      location: 'Universitas Indonesia',
+      fixed: true,
+      topics: {
+        'social_science': {
+          'linguistics': 'Linguistics',
+          'politics': 'Politics',
+        },
+        'humanities': {
+          'philosophy': 'philosophy'
+        },
+      }
+    },
+    '7006569730': {
+      title: 'Redux with React',
+      description: `
+      # The first non-justificational philosophy of criticism in the history of philosophy
+
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+      est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
+      modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+      veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+      commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+
+      ## See you there!
+      `,
+      date: '2020-08-21',
+      time: 'T10:00:38.117Z',
+      duration: 45,
+      location: 'Singapore Institute of Management',
+      fixed: true,
+      topics: {
+        'engineering': {
+          'computer_science': 'Computer Science',
+        }
+      }
+    },
+    '8474312706': {
+      title: 'Legal aspects of starting your own small business',
+      description: `
+      # The first non-justificational philosophy of criticism in the history of philosophy
+
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+      est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
+      modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+      veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+      commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+
+      ## See you there!
+      `,
+      date: '2020-09-01',
+      time: 'T10:00:38.117Z',
+      duration: 60,
+      location: 'Binus University',
+      fixed: true,
+      topics: {
+        'business': {
+          'entrepreneurship': 'Entrepreneurship',
+          'administration': 'Administration',
+        }
+      },
+      password: '1H4S2fRdzcdpfQrK',
+    },
+  },
+  topics: {
+    'social_science': {
+      'linguistics': 'Linguistics',
+      'law': 'Law',
+      'politics': 'Politics',
+      'sociology': 'Sociology',
+      'journalism': 'Journalism',
+    },
+    'business': {
+      'entrepreneurship': 'Entrepreneurship',
+      'administration': 'Administration',
+      'law': 'Business Law'
+    },
+    'engineering': {
+      'electrical': 'Electrical',
+      'mechanical': 'Mechanical',
+      'computer_science': 'Computer Science',
+      'chemical': 'Chemical',
+    },
+    'science': {
+      'physics': 'Physics',
+      'maths': 'Maths',
+      'biology': 'Biology',
+      'chemistry': 'Chemistry',
+    },
+    'arts': {
+      'illustration': 'Illustration',
+      'performing': 'Performing Arts',
+      'music': 'Music',
+      'modern': 'Modern Art',
+      'art_history': 'Art History',
+    },
+    'humanities': {
+      'philosophy': 'Philosophy',
+      'history': 'History',
+      'literature': 'Literature',
+      'gender_studies': 'Gender Studies',
+    },
+    'other': 'Other',
+  }
+}
